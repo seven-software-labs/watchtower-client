9341ca36e373e2f349308f136386fc0e88eb55cc
Jonathan Tordesillas <jonathan.tordesillas@5align.com>
2021-03-03 18:31:24 +0800

    Add table wrapping.

--- src/components/table-data.vue
+++ src/components/table-data.vue
@@ -1,5 +1,5 @@
 <template>
-    <td class="px-4 sm:px-6 py-4 text-sm bg-white whitespace-nowrap">
+    <td class="px-4 sm:px-6 py-4 text-sm bg-white whitespace-wrap">
         <slot></slot>
     </td>
 </template>
