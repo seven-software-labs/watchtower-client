eb195b45f3c9cc70babd0a2f6c43bb667e8a3507
Jonathan Tordesillas <yamato.takato@gmail.com>
2024-02-07 16:30:57 +0000

    Use form groups.

--- src/components/form/group.vue
+++ src/components/form/group.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="form-group w-full">
+    <div class="form-group w-full space-y-1">
         <slot></slot>
     </div>
 </template>
--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -9,41 +9,26 @@
         </template>
 
         <form class="space-y-6" action="#" method="POST" @submit.prevent="submitLoginForm(loginForm)">
-            <div>
+            <x-form-group>
                 <x-form-label for="username">
                     Organization Subdomain
                 </x-form-label>
                 <x-form-input type="text" v-model="loginForm.organization_subdomain" required :disabled="isLoading"/>
-            </div>
+            </x-form-group>
 
-            <div>
+            <x-form-group>
                 <x-form-label for="username">
                     Email address
                 </x-form-label>
                 <x-form-input type="email" v-model="loginForm.username" required :disabled="isLoading"/>
-            </div>
+            </x-form-group>
 
-            <div>
+            <x-form-group>
                 <x-form-label for="password">
                     Password
                 </x-form-label>
                 <x-form-input type="password" v-model="loginForm.password" required :disabled="isLoading"/>
-            </div>
-
-            <div class="flex items-center justify-between">
-                <div class="flex items-center">
-                    <input id="remember_me" name="remember_me" type="checkbox" class="h-4 w-4 text-primary-600 focus:ring-blue-500 border-gray-300 rounded">
-                    <label for="remember_me" class="ml-2 block text-sm text-gray-900">
-                    Remember me
-                    </label>
-                </div>
-
-                <div class="text-sm">
-                    <a href="#" class="font-medium text-primary-600 hover:text-primary-500">
-                    Forgot your password?
-                    </a>
-                </div>
-            </div>
+            </x-form-group>
 
             <div>
                 <x-button type="submit" color="primary" class="w-full flex items-center justify-center" :disabled="isLoading">
