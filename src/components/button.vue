b9895ac996f6798ecc25316d4411eed8a7142b40
Jonathan Tordesillas <yamato.takato@gmail.com>
2024-02-07 14:03:18 +0000

    Update classes based on updated TailwindCSS.

--- src/components/button.vue
+++ src/components/button.vue
@@ -59,15 +59,14 @@ export default {
     },
     setup(props) {
         // Base classes for the component.
-        const baseClasses = "x-button inline-flex items-center justify-center border shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed disabled:border-4";
+        const baseClasses = "x-button inline-flex items-center justify-center border shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed";
 
         // List of available colors for the component.
         const colors = {
-            primary: "border-primary-500 text-white bg-gradient-to-r from-primary-500 to-primary-600 hover:from-primary-600 focus:ring-primary-500",
-            success: "border-success-500 text-white bg-gradient-to-r from-success-500 to-success-600 hover:from-success-600 focus:ring-success-500",
+            primary: "border-primary-500 text-white bg-gradient-to-r from-primary-500 to-primary-600 hover:from-primary-600 hover:to-primary-600 focus:ring-primary-500",
+            success: "border-success-500 text-white bg-gradient-to-r from-success-500 to-success-600 hover:from-success-600 hover:to-success-600 focus:ring-success-500",
             danger: "border-red-500 text-white bg-red-500 hover:bg-red-600 focus:ring-red-600",
-            white: "bg-white border-gray-300 text-gray-900 hover:bg-gray-200 focus:ring-gray-900",
-            blue: "border-blue-500 text-white bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 focus:ring-blue-600",
+            white: "bg-white border-gray-300 text-gray-900 hover:bg-gray-200 focus:ring-gray-900"
         };
 
         // List of available sizes for the component.
